4c0e04d38a9e5dce3f17e32d4abfbe23fb2bb31f
Brian Sunter <bsunter@Brians-MacBook-Pro-3.local>
2017-01-12 00:49:37 -0800

    Added chip input

--- src/cljs/cards/views/add_card.cljs
+++ src/cljs/cards/views/add_card.cljs
@@ -1,5 +1,6 @@
 (ns cards.views.add-card
   (:require
+   [cljsjs.material-ui-chip-input]
    [cljsjs.material-ui]
    [cljs-react-material-ui.core :refer [get-mui-theme color]]
    [cljs-react-material-ui.reagent :as ui]
@@ -7,10 +8,12 @@
    [re-frame.core :as re-frame]
    [reagent.core :as r]))
 
+(def chip-input (r/adapt-react-class js/MaterialUIChipInput))
+
 (defn face-view
   [name hint]
   [ui/paper
-   {:z-depth 3
+   {:z-depth 2
     :style {
             :margin 10
             :padding 10
@@ -18,8 +21,10 @@
    [ui/text-field {:name name :floating-label-text hint
                    :full-width true
                    :multi-line true
-                   :style {:font-size 28
-                           }}]])
+                   :style {:font-size 28}}]])
+
+(def stub-languages
+  ["spanish" "english" "chinese" "japanese" "hindi" "italian" "german" "russian" "arabic" "integerated chinese 1"])
 
 (defn add-card-view
   []
@@ -29,10 +34,17 @@
    [ui/paper {:z-depth 3
               :style {:margin 10
                       :margin-top 40}}
+    [chip-input {:dataSource (clj->js stub-languages)
+                 :fullWidth true
+                 :style {:margin 10}
+                 }]]
+   [ui/paper {:z-depth 3
+              :style {:margin 10
+                      :margin-top 40}}
     [ui/raised-button {:primary true
                        :full-width true
                        }[:a {:style {:color "white"
+                                     :padding 30
                                      :margin 40
                                      :font-size 28
-                                     }}"Create Card"]]]]
-  )
+                                     }}"Create Card"]]]])
